4a605be4f5cd960f7ef70d5159210c728819e0bd
Camilla Mai Ryskjær <CARY@zealand.dk>
2024-02-23 10:49:28 +0100

    Add project files.

---
+++ StaticExampleInUse/Order.cs
@@ -0,0 +1,33 @@
+﻿using System;   
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExampleInUse
+{
+    internal class Order
+    {
+        int _id;
+        DateTime _date;
+        private static int idCounter = 0;
+
+        public int Id { get => _id; private set => _id = value; }
+        public DateTime Date { get => _date; private set => _date = value; }
+
+     
+        public Order()
+        {
+            Id = idCounter;
+            Date = DateTime.Now;
+            idCounter++;
+        }
+
+
+        public override string ToString()
+        {
+            return "Ordre-id: "+Id +": Dato: "+Date.ToString("dd-MM-yy : kl: HH:mm:ss:fff");
+        }
+
+    }
+}
---
+++ StaticExampleInUse/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace StaticExampleInUse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Order o1 = new Order();
+            Console.WriteLine(o1.ToString());
+            Thread.Sleep(100);
+            Order o2 = new Order();
+            Console.WriteLine(o2.ToString());
+            Thread.Sleep(100);
+            Order o3 = new Order();
+            Console.WriteLine(o3.ToString());
+            Console.ReadKey();
+        }
+    }
+}
